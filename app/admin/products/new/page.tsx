b6921928215e8a4bac01886e45bbd71d2288e04f
Danilo Urrego <daniloud2809@gmail.com>
2025-02-09 16:43:18 -0500

    Feature: Create & Update Products

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,11 +1,23 @@
+import { AddProductForm } from "@/components/products/AddProductForm"
+import { ProductForm } from "@/components/products/ProductForm"
 import { ProductsTable } from "@/components/products/ProductsTable"
+import { GoBackButton } from "@/components/ui/GoBackButton"
 import { Heading } from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
 
-export const NewProductPage = async () => {
+const NewProductPage = async () => {
 
   return (
     <>
+      <Heading>Nuevo Producto</Heading>
+
+      <GoBackButton />
+
+      <AddProductForm>
+        <ProductForm />
+      </AddProductForm>
     </>
   )
 }
+
+export default NewProductPage
